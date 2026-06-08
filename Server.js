3dd1161468c93ba5a781278a8970ee68fa773bc3
swayam sherekar <swayamsherekar90@gmail.com>
2026-06-08 23:21:02 +0530

    Add files via upload

---
+++ Server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Chronical AI Server Running 🚀");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
